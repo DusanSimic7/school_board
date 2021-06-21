86afd72b2e6b64d2785d0df7a7e056b24cd97446
Dusan Simic <dusansimic202gmail.com>
2021-06-21 23:41:01 +0200

    add xml

--- StudentController.php
+++ StudentController.php
@@ -74,9 +74,35 @@ if(isset($_GET['student'])){
 
         $new_student = $student->find($st_id);
 
-        $dataJSON = json_encode($new_student);
+        $xmlDoc = new DOMDocument();
 
-        echo $dataJSON;
+        $root = $xmlDoc->appendChild($xmlDoc->createElement("user_info"));
+        $root->appendChild($xmlDoc->createElement("Name", $new_student->name));
+        $root->appendChild($xmlDoc->createElement("Board", $new_student->board));
+        $root->appendChild($xmlDoc->createElement("Grades", $new_student->grades));
+        $root->appendChild($xmlDoc->createElement("Average", $new_student->average));
+        $root->appendChild($xmlDoc->createElement("Result", $new_student->result));
+
+
+        header("Content-Type:text/plain");
+
+        $xmlDoc->formatOutput = true;
+
+
+        $file_name = str_replace(' ',  '', $new_student->name.'.xml');
+
+        $xmlDoc->save("files/".$file_name);
+
+
+
+
+
+        $feed = file_get_contents("files/".$file_name);
+        $items = simplexml_load_string($feed);
+
+
+
+        var_dump($items);
 
     }
 
